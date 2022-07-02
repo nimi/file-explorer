0b8bc347141527f7e96436965e20f39701e98de9
Nicholas Mitchell <nicholas@netlify.com>
2022-07-01 21:45:40 -0500

    Add layout styles with css props

--- modules/file-explorer-app.js
+++ modules/file-explorer-app.js
@@ -2,9 +2,45 @@ import "../modules/directory-content.js"
 import "../modules/directory-tree.js"
 
 export class FileExplorerApp extends HTMLElement {
+  static styles = `
+    :host {
+      display: flex;
+      height: 100vh;
+      flex-direction: column;
+    }
+    :host > div {
+      display: flex;
+      flex: 1;
+      overflow: hidden;
+    }
+    nav {
+      flex: 1;
+      border-right: solid 1px var(--border-color);
+      padding: var(--size-md);
+      position: relative;
+    }
+    main {
+      position: relative;
+      padding: var(--size-sm) 0;
+      width: var(--size-xl);
+    }
+
+    @media (min-width: 768px) {
+      nav {
+        flex: 0 0 var(--size-side-nav);
+      }
+
+      main {
+        width: auto;
+        flex: 1;
+      }
+    }
+  `
+
   connectedCallback() {
     this.attachShadow({ mode: "open" })
     this.shadowRoot.innerHTML = `
+      <style>${FileExplorerApp.styles}</style>
       <div>
         <nav>
           <directory-tree></directory-tree>
